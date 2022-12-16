cfa135281a612f7c34dc44c18811f9efecf61cf5
Samuael Ketema <lijsamuael@gmail.com>
2022-12-17 00:54:18 +0300

    production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCF0n6n9MEmpQwEQijlkGST001ZG2VQWIo',
+    authDomain: 'zelalem-trading.firebaseapp.com',
+    projectId: 'zelalem-trading',
+    storageBucket: 'zelalem-trading.appspot.com',
+    messagingSenderId: '506555958505',
+    appId: '1:506555958505:web:c6018b034f083321735c78',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
     apiKey: 'AIzaSyCF0n6n9MEmpQwEQijlkGST001ZG2VQWIo',
     authDomain: 'zelalem-trading.firebaseapp.com',
